ec41c7458fb2a3216d36b108ab3455f7483bd716
Luke Richardson <luke.richardson@firstwave.com>
2025-10-31 15:58:00 +1000

    Initial commit for Nuxt docs site

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+  ],
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-04-03',
+})
